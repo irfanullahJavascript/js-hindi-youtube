c35f425cc9280a474dddafb8a4dd20511b5789fe
irfanullahJavascript <148108345+irfanullahJavascript@users.noreply.github.com>
2023-10-16 17:57:05 +0000

    =after variable and datatype of javascript

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId=1234567
+let eamil="irfanullah@gmail.com"
+var password="1223456"
+cityname="mianwali"
+eamil="irfanullahkhan@gmail.com"
+password="5362362362"
+cityname="Rawalpindi"
+let accountstate; //undefine bc y scopeoue not assign value yet.
+
+/*
+prefer not to use var
+beacuse of block scope and function
+*/
+console.table([accountId,eamil,password,cityname,accountstate]);
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,13 @@
+"use strict"; //treat all js code as new virsions
+//permitove data type
+//numbers =>2 to power 52
+//bigint 
+//string =""
+//boolean true/false
+//null standalone value
+//undefine no assign value yet
+//symobol =>i=use for uniqueness
+
+//object
+console.log(typeof null);//objects
+console.log(typeof undefine); //undefine
