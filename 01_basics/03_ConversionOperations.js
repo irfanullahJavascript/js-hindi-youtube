64d8ac68517d0b1e54c582ffc6ccb990031c529f
irfanullahJavascript <148108345+irfanullahJavascript@users.noreply.github.com>
2023-10-18 06:34:13 +0000

    =comparisons of different type of data

--- 01_basics/03_ConversionOperations.js
+++ 01_basics/03_ConversionOperations.js
@@ -25,5 +25,21 @@ let logedIn=Boolean(login)
  //String Conversions
  let someNumber =33
  let stringNumber=String(someNumber)
- console.log(stringNumber);
- console.log(typeof stringNumber);
+//  console.log(stringNumber);
+//  console.log(typeof stringNumber);
+
+//************************************************Operations********************************************************************
+// Number 
+let value=3
+let negValue=-value
+// console.log(negValue);
+//string
+let str1='irfan'
+let str2=" ullah"
+let str3=str1+str2
+// console.log(str3);
+let gameCounter=100
+gameCounter++
+console.log(gameCounter);
+//study purposes
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
---
+++ 01_basics/04_Comparision.js
@@ -0,0 +1,26 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2<=1);
+// console.log(2==1);
+// console.log(2!=1);
+//return you true false value .
+
+//console.log("2">1);//true because javascript automatocally convert string into number But always compare same datatype to achive axect same reasult
+//console.log("02">1);//true  ||but type script not allow you to compare toe doffernt data type ||typescri[t goive yo dtrict rule 
+
+
+console.log(null>0);//false
+console.log(null<0);//false
+console.log(null==0);//strict equal false 
+console.log(null>=0); //true lose equal
+
+console.log(undefined>0);false
+console.log(undefined<0);false
+console.log(undefined>=0);false
+
+// strict conversion ===    compare both datatype as well as value || console.log("2==="2) //false
+//lose coversion ==    compare value and give you results || console.log("2"==2);//true
+
+
+// Note: Always use same data type comparison to avoid defficulities 
