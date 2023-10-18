942b51e3aa3c07aa61046d5a1c71f82114d36e25
irfanullahJavascript <148108345+irfanullahJavascript@users.noreply.github.com>
2023-10-18 05:27:25 +0000

    =conversions Operations

---
+++ 01_basics/03_ConversionOperations.js
@@ -0,0 +1,29 @@
+// let score="33qwq"
+// let score=null
+// let score=undefined
+// let score=true
+// let score="irfanullah"
+// console.log(typeof score);
+// console.log(typeof(score));
+// let valueInNumer=Number(score);
+// console.log(typeof valueInNumer);
+// console.log(valueInNumer);
+
+//33=>33
+//"33"=>NaN
+//null=>0
+
+
+// Boolean conversion 
+let login=1
+let logedIn=Boolean(login)
+// console.log(logedIn);
+ //1=>true,0=>false
+ //""=>false
+ //"irfanulah"=>true
+
+ //String Conversions
+ let someNumber =33
+ let stringNumber=String(someNumber)
+ console.log(stringNumber);
+ console.log(typeof stringNumber);
