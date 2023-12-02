c72ceb7bdf943e90cac42a4d68c1ea2626ebba7a
irfanullahJavascript <148108345+irfanullahJavascript@users.noreply.github.com>
2023-12-02 14:49:22 +0000

    arrray  in javascript

---
+++ 02_basics/01_array_js.js
@@ -0,0 +1,11 @@
+// 1-mathod array declaration 
+const myArray=[0,1,2,3,4] //same data array
+// 2-method array declaratiom  using new key word means through object in this way no need to add squir brakets
+const myArray2=new Array(1,2,3,4,5,6,7)  //this no need to add brakets it automatically convert into arrays
+
+// Note:: when we cosole this array like this const number =[1,3,5,6,7,8] =>This well give properties and proto type you can used 
+
+
+//Array Methods
+
+console.log(myArray)
