b4cc5f1955fedd3d876c4583f4200dbe89954f63
irfanullahJavascript <148108345+irfanullahJavascript@users.noreply.github.com>
2024-01-14 18:16:22 +0000

    heigh Order_loops

---
+++ 05_ittrations/02_Do_While_loop.js
@@ -0,0 +1,30 @@
+//Same work like for loop but syntet is different
+
+//print 1-20 vaule with 3 increment
+    // let i=1;
+    // while (i<=20) {
+    //     console.log(i)
+    //     i=i+2
+    // }
+
+//print  array using while loop
+    // let myArray=['A','B','C'];
+    // let arr=0;
+    // while (arr<myArray.length) {
+    //     console.log(`Value is ${myArray[arr]}`);
+    //     arr=arr+1;
+    // }
+
+
+
+
+
+
+
+// __________________________Do-while_loop____________________________
+//in this loop 1st itteration always execuite and even the coindtion is wrong beaces 1st code execuite and than check condtion and increment
+let score=11;
+do {
+    console.log(`Score is ${score}`)
+    
+} while (score<=10);
---
+++ 05_ittrations/03_hiegh_Order_loop.js
@@ -0,0 +1,5 @@
+//there are several type of itteraion used for arrays like forof loop , forin loop ,foreach loop etc not lets see one by one
+
+
+ //youe can all these loop on string ,array objects etc.what you want ot apply so lets see thse loops one by one.
+ 
